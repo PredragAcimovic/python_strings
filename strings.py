d20e6ed8f1222603e0f327738003a5927adde25c
Predrag <pedjaacimovic44@gmail.com>
2023-12-12 18:59:07 +0100

    using variables in strings

--- strings.py
+++ strings.py
@@ -4,3 +4,17 @@ print(name.title())
 name = "Ada lovelace"
 print(name.upper())
 print(name.lower())
+#using variables in strings
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+print(full_name)
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+print(f"Hello, {full_name.title()}!")
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+message = f"Hello, {full_name.title()}!"
+print(message)
